911f85547f203942c421e77a292b52024a19a3ce
aditya pratap <pratapaditya84@gmail.com>
2023-09-28 19:22:52 +0530

    ..

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ app.use((req,res,next)=>{
   next()
 })
 app.use(express.json())
-app.use(cors({
-    origin:"http://localhost:5173",
-}))
+app.use(cors())
 app.use(cookieParser())
 
 const storage = multer.diskStorage({
