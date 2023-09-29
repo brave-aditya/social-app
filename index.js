7b5f40ab3262b02e318fbfdbcbd15ef9a9b0cfa2
aditya pratap <pratapaditya84@gmail.com>
2023-09-29 21:07:45 +0530

    ,,

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import relationshipRoutes from './routes/relationships.js'
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import multer from "multer";
+const axios = require('axios');
+const path = require('path');
 import { db } from "./connect.js";
 const app = express();
 const PORT = process.env.port||8808;
@@ -25,22 +27,36 @@ let corsOptions = {
 app.use(cors(corsOptions))
 app.use(cookieParser())
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '../client/public/upload')
-  },
-  filename: function (req, file, cb) {
-    
-    cb(null,Date.now()+ file.originalname)
+app.use(express.static('public'));
+
+// Configure Multer to handle file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// Set up a route to handle file uploads
+app.post('/api/upload', upload.single('image'), async (req, res) => {
+  try {
+    // Send the uploaded image to ImgBB
+    const response = await axios.post('https://api.imgbb.com/1/upload', null, {
+      params: {
+        key: 'b99e1b7e44deb3985e33be22d597e53f', // Replace with your ImgBB API key
+      },
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      data: {
+        image: req.file.buffer.toString('base64'),
+      },
+    });
+
+    // Handle the ImgBB API response here
+    const imageUrl = response.data.data.url;
+    res.json({ imageUrl });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while uploading the image.' });
   }
-})
-
-const upload = multer({ storage: storage })
-
-app.post("/api/upload",upload.single("file"),(req, res)=>{
-  const file =req.file;
-  res.status(200).json(file.filename)
-})
+});
 
 db.connect(function(err) {
   if (err) throw err;
