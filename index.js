aa6ebf38cefffe3b0b31943eb3b917f4c66c5383
aditya pratap <pratapaditya84@gmail.com>
2023-09-29 12:53:54 +0530

    ,

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use((req,res,next)=>{
 app.use(express.json())
 let corsOptions = {
   origin : 'https://social-umber.vercel.app',
+  credentials: true,
 }
 app.use(cors(corsOptions))
 app.use(cookieParser())
